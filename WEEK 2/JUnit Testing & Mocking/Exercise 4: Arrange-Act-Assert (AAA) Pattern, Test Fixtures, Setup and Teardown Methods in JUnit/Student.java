7739c92f8f4d30b88a83f18d3a06ad2eecb0933b
nithyaa-priyaa <167992302+nithyaa-priyaa@users.noreply.github.com>
2025-06-29 23:35:49 +0530

    Create Student.java

---
+++ WEEK 2/JUnit Testing & Mocking/Exercise 4: Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup and Teardown Methods in JUnit/Student.java
@@ -0,0 +1,48 @@
+public class Student {
+    private int id;
+    private String name;
+    private double grade;
+    
+    public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getGrade() {
+        return grade;
+    }
+    
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Student student = (Student) obj;
+        return id == student.id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "', grade=" + grade + "}";
+    }
+}
